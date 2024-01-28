96c9acc2cb1399217b22335e9fdaf197822cda6c
Nick Owens <mischief@offblast.org>
2024-01-28 15:02:10 -0800

    examples: fix sol example
    when using a global sol::state, the constructor seems to crash if TLS is
    used by sol. perhaps there's no memory allocated for tls or it's
    discarded by the linker? :shrug:

--- examples/basic_sol.ino
+++ examples/basic_sol.ino
@@ -1,3 +1,5 @@
+// required to be defined when using a global sol::state
+#define SOL_NO_THREAD_LOCAL 1
 #include <sol/sol.hpp>
 
 sol::state lua;
@@ -13,13 +15,24 @@ static void debug_hook(lua_State *state, lua_Debug *dbg){
 void setup(){
 	Serial.begin(115200);
 
-	lua["millis"] = millis();
+	// for print
+	lua.open_libraries(sol::lib::base);
 
+	// custom function
+	lua["millis"] = millis;
+
+	// setup debug hook callback
 	lua_sethook(lua.lua_state(), debug_hook, LUA_MASKCOUNT, 50000);
 }
 
 void loop(){
-	lua.script("print(millis())");
+	// catch errors
+	auto result = lua.safe_script("print(millis())");
+	if (!result.valid()) {
+		sol::error err = result;
+		Serial.println(err.what());
+	}
+
 	delay(1000);
 }
 
